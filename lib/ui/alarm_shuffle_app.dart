9e280e61cf1d53929e5226fe018ae84231e38ed7
camillelnne <camille.lannoye@epfl.ch>
2025-01-20 14:45:26 +0100

    feat(app): ad dark mode

--- lib/ui/alarm_shuffle_app.dart
+++ lib/ui/alarm_shuffle_app.dart
@@ -3,15 +3,27 @@ import "package:flutter/material.dart";
 
 class AlarmShuffleApp extends StatelessWidget {
   const AlarmShuffleApp({super.key});
+  static const title = "Alarm Shuffle";
+  static const color = Color.fromARGB(255, 52, 37, 168);
+
+
 
   @override
   Widget build(BuildContext context) {
+    final brightness = MediaQuery.platformBrightnessOf(context);
+
     return MaterialApp(
-      title: "Alarm Clock",
+      title: title,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         useMaterial3: true,
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: color,
+          brightness: brightness,
+        ),
+        
       ),
+      
       home: const AlarmScreen(),
     );
   }
